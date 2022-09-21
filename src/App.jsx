821d9131f4916883c061205c5ad871fb5a2a959e
TheoToni <32997269+TheoToni@users.noreply.github.com>
2022-09-21 21:24:29 +0200

    Creating random numbers saving in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import { useState } from "react";
 import "./App.css";
+import Die from "./components/Die";
 
 function App() {
+  const [dices, setDices] = useState(generateDices());
+
+  function generateDices() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  const diceValues = dices.map((dice) => {
+    return <Die value={dice}></Die>;
+  });
+
   return (
     <div className="App">
       <div className="main">
@@ -10,6 +25,7 @@ function App() {
           Roll until all dice are same. Click each die to freeze it at its
           current value between rolls.
         </p>
+        <div className="grid-wrapper">{diceValues}</div>
       </div>
     </div>
   );
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./Die.module.css";
 
-function Die() {
-  return <div>Die</div>;
+function Die(props) {
+  return (
+    <div className={styles.die}>
+      <h2>{props.value}</h2>
+    </div>
+  );
 }
 
 export default Die;
