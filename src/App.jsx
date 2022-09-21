bcfb5feb9266c3e6e9b4c693e926172ed912b6a2
TheoToni <32997269+TheoToni@users.noreply.github.com>
2022-09-21 08:10:42 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        <h1>Tenzies</h1>
+        <p>
+          Roll until all dice are same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Die.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Die() {
+  return <div>Die</div>;
+}
+
+export default Die;
